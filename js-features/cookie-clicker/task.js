063901066093e39cc1c59e2bfa9ca5e4eeef429c
Anna Chumakova <chumakova3008@gmail.com>
2019-10-31 14:53:37 +0500

    готова вторая задача по звездочкой

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,32 +2,43 @@ const element = document.getElementById("cookie")
 
 let begine = document.getElementById("clicker__counter");
 
+let loadTime;
 let startTime;
 let start = 0;
 let endTime = 0;
 let end = 0;
 
 let speed = document.getElementById("speed");
-console.log(`speed.textContent ${speed.textContent}`);
+
+window.addEventListener("load", timeLoad);
+
+function timeLoad() {
+	let load = new Date;
+	loadTime = load.getTime();
+	return loadTime;
+}
 
 function clicker() {
 	begine.textContent ++;
 
-	if (begine.textContent % 2 == 0) {
+	if (begine.textContent == 1) {
+		element.width = 150;
+		startTime = new Date;
+		startFirstClick = startTime.getTime();
+		end = loadTime;
+	
+	if (begine.textContent % 2 !== 0) {
 		element.width = 150;
 		startTime = new Date;
 		start = startTime.getTime();
-		console.log(`start ${start}`);
 	} else {
 		element.width = 200;
 		endTime = new Date;
 		end = endTime.getTime();
-		console.log(`end ${end}`);
 	}
 	
 	let time;
 	time = end - start;
-		console.log(`time ${time}`);
 
 	let newTime;
 	if (time < 0) {
@@ -36,7 +47,6 @@ function clicker() {
 		newTime = time;
 	}
 
-	console.log(`newTime ${newTime}`);
 	speed.textContent = newTime / 1000;
 }
 
