2d68e60db1091283f09113cda467afd5be2224f2
Anna Chumakova <chumakova3008@gmail.com>
2019-11-01 17:26:23 +0500

    Решена 3задача из 1Домашнего задания

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,39 @@
+let hole = [];
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
+dead.textContent = 0;
+lost.textContent = 0;
+let victory = 0;
+let defeat = 0;
+
+function getHole() {
+	for (let i = 1; i < 10; i ++) {
+		hole.push(document.getElementById(`hole${i}`));
+	}
+	return hole;
+}
+
+getHole();
+
+for (let i = 0; i < 9; i ++) {
+	hole[i].onclick = sniper;
+	function sniper() {
+		if (hole[i].classList.contains("hole_has-mole")) {
+			dead.textContent ++ ;
+			console.log(dead.textContent);
+		} else {
+			lost.textContent ++ ;
+			console.log(lost.textContent);
+		}
+		
+		if (dead.textContent == 4) {
+			alert("4 бобра убито! Вы не зачислены в GreenPeace, но Вы победили!");
+			dead.textContent = 0;
+			lost.textContent = 0;
+		} else if (lost.textContent == 4) {
+			alert("Вы промахнулись 4 раза. Вы проиграли!");
+			dead.textContent = 0;
+			lost.textContent = 0;
+		}
+	}
+}
