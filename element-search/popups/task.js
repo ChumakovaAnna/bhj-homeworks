b7a61107c34d613995a0ea6c18a311ff584dba32
Anna Chumakova <chumakova3008@gmail.com>
2019-11-05 23:51:41 +0500

    решена задача первая задача

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,29 +1,26 @@
 let modal = document.getElementById("modal_main");
-console.log(modal);
 modal.classList.add("modal_active");
 
 let allCloses = document.getElementsByClassName("modal__close");
-console.log(allCloses);
-let button = document.getElementsByClassName("show-success");
-console.log(button);
+let crossFirst = allCloses.item(0);
+// let button = document.getElementsByClassName("show-success");
+let button = allCloses.item(1);
+let crossSecond = allCloses.item(2);
 let modalSucces = document.getElementById("modal_success");
-console.log(modalSucces);
 
-let faq;
-let oneClose;
-for (let i = 0; i < allCloses.length; i ++) {
-	console.log(i);
-	oneClose = allCloses.item(i);
-	console.log(oneClose);
-	faq = oneClose.classList.contains("show-success");
-	oneClose.onclick = function () {
-		if (faq) {
-			modal.classList.remove("modal_active");
-			modalSucces.classList.add("modal_active");
-			console.log("rkbr");
-		} else {
-			modal.classList.remove("modal_active");
-			console.log("jj");
-		}
-	}
-};
+function closeBlock() {
+	modal.classList.remove("modal_active");
+}
+
+function closeAndOpen() {
+	modal.classList.remove("modal_active");
+	modalSucces.classList.add("modal_active");
+}
+
+function closeSuccess() {
+	modalSucces.classList.remove("modal_active");
+}
+
+crossFirst.onclick = closeBlock;
+button.onclick = closeAndOpen;
+crossSecond.onclick = closeSuccess;
