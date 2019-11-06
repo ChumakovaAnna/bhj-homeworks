abbb310f1ebec9bc6e86de7f0c3c5ec32cf1362b
Anna Chumakova <chumakova3008@gmail.com>
2019-11-06 17:14:15 +0500

    Убрала console.log

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,19 +1,14 @@
 let menuLinks = document.querySelectorAll("a.menu__link");
-console.log(menuLinks);
 let about = menuLinks.item(1);
 let servieces = menuLinks.item(5);
 
 
 let menuSub = document.querySelectorAll("ul.menu_sub");
-console.log(menuSub);
 
 let menuSubAbout = menuSub.item(0);
-console.log(menuSubAbout);
 let menuSubServices = menuSub.item(1);
-console.log(menuSubServices);
 
 menuLinks.forEach( function(ele) {
-	console.log(ele);
 	ele.onclick = function() {
 		if (ele == about) {
 			menuSubAbout.classList.add("menu_active");
