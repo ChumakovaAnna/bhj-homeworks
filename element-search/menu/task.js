0a8987eed7cf067d8f26a2575687d31adeb706fa
Anna Chumakova <chumakova3008@gmail.com>
2019-11-06 17:09:46 +0500

    решила 2 задачу

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,5 +1,31 @@
-let menuLink = document.querySelectorAll("a.menu__link");
-console.log(menuLink);
+let menuLinks = document.querySelectorAll("a.menu__link");
+console.log(menuLinks);
+let about = menuLinks.item(1);
+let servieces = menuLinks.item(5);
+
 
 let menuSub = document.querySelectorAll("ul.menu_sub");
 console.log(menuSub);
+
+let menuSubAbout = menuSub.item(0);
+console.log(menuSubAbout);
+let menuSubServices = menuSub.item(1);
+console.log(menuSubServices);
+
+menuLinks.forEach( function(ele) {
+	console.log(ele);
+	ele.onclick = function() {
+		if (ele == about) {
+			menuSubAbout.classList.add("menu_active");
+			menuSubServices.classList.remove("menu_active");
+			return false
+		} else if (ele == servieces) {
+			menuSubServices.classList.add("menu_active");
+			menuSubAbout.classList.remove("menu_active");
+			return false
+		} else {
+			menuSubServices.classList.remove("menu_active");
+			menuSubAbout.classList.remove("menu_active");
+		}
+	}
+});
