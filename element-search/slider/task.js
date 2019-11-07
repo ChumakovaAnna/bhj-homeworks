9bc030fe64e044a6e3a483bef13fb7a8c4cee5cd
Anna Chumakova <chumakova3008@gmail.com>
2019-11-07 14:15:23 +0500

    Решена задача 3 со свездочкой.

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,29 +1,98 @@
 let arrows = document.querySelectorAll("div.slider__arrow");
-console.log(arrows);
 let leftArrow = arrows.item(0);
 let rightArrow = arrows.item(1);
 
 let images = document.querySelectorAll("img.slider__image");
-console.log(images);
-let dots = document.querySelectorAll("slider__dot");
+let index = 0;
+let activeImage;
+let parentActiveImage;
+let dots = document.querySelectorAll("div.slider__dot");
+let activeDot = dots.item(index);
+activeDot.classList.add("slider__dot_active");
 
-function previous() {
-		images.forEach( function (ele, index) {
-		console.log(ele);
-		console.log(index);
-		let parentEle = ele.parentElement;
-		if (parentEle.classList.contains("slider__item_active")) {
-			parentEle.classList.remove("slider__item_active");
-			console.log(parentEle);
-			if (index == 0) {
-				index = images.length;
-			}
-			let previousImag = images.item(index - 1);
-			let parentPrevious = previousImag.parentElement;
-			parentPrevious.classList.add("slider__item_active");
-		}
+function removeActive() {
+	activeImage = images.item(index);
+	parentActiveImage = activeImage.parentElement;
+	if (parentActiveImage.classList.contains("slider__item_active")) {
+		parentActiveImage.classList.remove("slider__item_active");
 	}
-	);
 }
 
-leftArrow.onclick = previous;
+function changeIndexMinus() {
+	index --;
+	if (index < 0) {
+		index = images.length - 1;
+	}
+}
+
+function changeIndexPlus() {
+	index ++;
+	if (index >= images.length) {
+		index = 0;
+	}
+}
+
+function changeActive() {
+	let newImage = images.item(index);
+	let parentNewImage = newImage.parentElement;
+	parentNewImage.classList.add("slider__item_active");
+}
+
+function removeDot() {
+	dots.forEach(function (dot, index) {
+		let activeDot = dots.item(index);
+		if (activeDot.classList.contains("slider__dot_active")) {
+			activeDot.classList.remove("slider__dot_active");
+		}
+	});
+}
+
+function changeDot() {
+	activeDot = dots.item(index);
+	activeDot.classList.add("slider__dot_active");
+}
+
+function changeBack() {
+	removeActive();
+	removeDot();
+	changeIndexMinus();
+	changeDot();
+	changeActive();
+}
+
+function changeForward() {
+	removeActive();
+	removeDot();
+	changeIndexPlus();
+	changeDot();
+	changeActive();
+}
+
+leftArrow.onclick = changeBack;
+rightArrow.onclick = changeForward;
+	
+dots.forEach(function (dot) {
+	dot.onclick = clickDot;
+	
+	function clickDot(event) {
+		let arrDots = Array.from(dots);
+		let index = arrDots.indexOf(event.currentTarget);
+		
+		removeDot()
+
+		let activeDot = dots.item(index);
+		activeDot.classList.add("slider__dot_active");
+
+		images.forEach(function (imag, index) {
+			activeImage = images.item(index);
+			parentActiveImage = activeImage.parentElement;
+			if (parentActiveImage.classList.contains("slider__item_active")) {
+				parentActiveImage.classList.remove("slider__item_active");
+			}
+		});
+
+		let newImage = images.item(index);
+		let parentNewImage = newImage.parentElement;
+		parentNewImage.classList.add("slider__item_active");
+	}
+});
