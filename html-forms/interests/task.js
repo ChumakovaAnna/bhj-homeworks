7925d89df59004703f20e942b6a31df2bfa7e380
Anna Chumakova <chumakova3008@gmail.com>
2020-02-16 00:22:07 +0500

    Выполнены все задания

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,32 +1,64 @@
 const allCheckbox = document.querySelectorAll(".interest__check");
 
-allCheckbox.forEach(function (ele) {
-	ele.addEventListener("change", () => {
-		console.log(ele);
-		let allParentsCheckbox = [];
-		
-		function searchParentsCheckbox(element) {
-			const parent = element.parentElement;
-			console.log(parent);
+const changeCheckbox = (e) => {
+	const ele = e.target;
+	let allSiblings = [];
 
-			// search all parents
-			if (parent) {
-				
-				// search all parent's checkboxs
-				if (parent.classList.contains("interest")) {
-					const firstChildLabel = parent.firstElementChild;
-					const firstChildCheckbox = firstChildLabel.firstElementChild;
-					allParentsCheckbox.push(firstChildCheckbox);
-				};
-
-				searchParentsCheckbox(parent);
-			} else {
-				console.log(allParentsCheckbox);
-				return allParentsCheckbox;
-			}
+	const parent = (element) => element.closest(".interest");
+	
+	function searchPreviousSiblings(element) {
+		const previousSibling = parent(element).previousElementSibling;
+		if (previousSibling) {
+			const previousSiblingCheckbox = previousSibling.querySelector(".interest__check");
+			allSiblings.push({item: previousSiblingCheckbox, status: previousSiblingCheckbox.checked});
+			searchPreviousSiblings(previousSibling);
 		}
+	}
+	
+	function searchNextSiblings(element) {
+		const nextSibling = parent(element).nextElementSibling;
+		if (nextSibling) {
+			const nextSiblingCheckbox = nextSibling.querySelector(".interest__check");
+			allSiblings.push({item: nextSiblingCheckbox, status: nextSiblingCheckbox.checked});
+			searchNextSiblings(nextSibling);
+		} else {
+			allSiblings.push({item: ele, status: ele.checked});
+		}
+	}
 
-		searchParentsCheckbox(ele);
-	});
-})
+	function searchChildren(element) {
+		const allChildrenCheckbox = parent(element).querySelectorAll(".interest__check");
+		allChildrenCheckbox.forEach(e => {
+			e.indeterminate = false;
+			e.checked = allChildrenCheckbox[0].checked;
+		});
+	}
+	
+	function changeParentCheckbox(element) {
+		const parentSibling = parent(element).closest(".interests").previousElementSibling;
+		if (parentSibling) {
+			const parentCheckbox = parentSibling.querySelector(".interest__check");
+			const statusAllSiblings = allSiblings.filter(item => item.status === true);
+			if (statusAllSiblings.length === allSiblings.length) {
+				parentCheckbox.indeterminate = false;
+				parentCheckbox.checked = true;
+			} else if(statusAllSiblings.length === 0) {
+				parentCheckbox.indeterminate = false;
+				parentCheckbox.checked = false;
+			} else {
+				parentCheckbox.indeterminate = true;
+			}
 
+			changeParentCheckbox(parent(parentCheckbox));
+		}
+	}
+
+	searchPreviousSiblings(ele);
+	searchNextSiblings(ele);
+	changeParentCheckbox(ele);
+	searchChildren(ele);
+};
+
+allCheckbox.forEach(ele => {
+	ele.addEventListener("change", changeCheckbox, false);
+});
