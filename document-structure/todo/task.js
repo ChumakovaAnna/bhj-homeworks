337edc181544ef59a4b5b2f3afc7fe9590ae7813
Anna Chumakova <chumakova3008@gmail.com>
2020-03-01 18:28:23 +0500

    Изменена работа с localStorage

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,19 +3,10 @@ const taskButton = document.getElementById("tasks__add");
 const tasksList = document.getElementById("tasks__list");
 const savedTasks = []
 
-function changeStorage() {
-	localStorage.clear();
-	localStorage.setItem("tasks", tasksList.innerHTML);
-};
-
 const removeTask = (e) => {
 	const taskTitle = e.target.closest(".task").querySelector(".task__title");
-	console.log(taskTitle.textContent);
 	e.target.closest(".task").remove();
-	// не понимаю, почему не находится индекс
-	savedTasks.indexOf(taskTitle.textContent);
-	console.log(savedTasks.indexOf(taskTitle.textContent));
-	changeStorage();
+	savedTasks.splice(savedTasks.indexOf(taskTitle.innerText.trim()), 1);
 };
 
 const addTask = (e) => {
@@ -32,8 +23,6 @@ const addTask = (e) => {
 			tasksList.insertAdjacentHTML("beforeend", taskHTML);
 			savedTasks.push(taskInput.value);
 			taskInput.value = "";
-			console.log(savedTasks);
-			changeStorage();
 			e.preventDefault();
 		}
 	}
@@ -43,4 +32,33 @@ taskInput.addEventListener("keypress", addTask);
 taskButton.addEventListener("click", addTask);
 tasksList.addEventListener("click", removeTask);
 
-// window.addEventListener("load", () => tasksList.insertAdjacentHTML("beforeend", localStorage.getItem("tasks")));
+function loadTasks() {
+	console.log(localStorage.getItem("tasks"))
+	const memory = localStorage.getItem("tasks");
+	const arr = memory.split(",");
+	if (!arr[1] == "") {
+		arr.forEach((ele) => {
+			const div = document.createElement("div");
+			div.classList.add("task");
+			div.innerHTML = `
+				<div class="task__title">
+					${ele}
+				</div>
+				<a href="#" class="task__remove">&times;</a>`;
+			tasksList.appendChild(div);
+		});
+	}
+}
+
+window.addEventListener("load", loadTasks);
+
+function onunloadTasks() {
+	const allSavedTasks = document.querySelectorAll("task__title");
+	allSavedTasks.forEach((ele) => {
+		savedTasks.push(ele.innerText.trim());
+	});
+	localStorage.removeItem("tasks");
+	localStorage.setItem("tasks", savedTasks);
+}
+
+window.addEventListener("unload", onunloadTasks);
