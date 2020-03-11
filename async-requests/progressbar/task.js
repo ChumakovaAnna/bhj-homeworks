0e69847ff7f2932fad639038dfda9f99cf40741b
Anna Chumakova <chumakova3008@gmail.com>
2020-03-11 23:41:19 +0500

    Решена задача 3

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,35 @@
+const progressBar = document.getElementById("progress");
+progressBar.value = 0;
+const form = document.getElementById("form");
+
+function addValue(xhr) {
+	progressBar.value += 0.02;
+	console.log(xhr);
+}
+
+function addListener(xhr) {
+	xhr.addEventListener("loadstart", addValue);
+	xhr.addEventListener("progress", addValue);
+	xhr.addEventListener("load", () => {
+		alert("Загрузка завершена.");
+	});
+	xhr.addEventListener("error", () => {
+		alert("Error");
+	});
+	xhr.addEventListener("abort", () => {
+		alert("Abort");
+	});
+	xhr.addEventListener("loadend", () => {
+		progressBar.value = 0;
+	});
+}
+
+form.addEventListener("submit", (ele) => {
+	const formData = new FormData(form);
+	let request = new XMLHttpRequest();
+	addListener(request);
+	request.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php");
+	request.send(formData);
+
+	ele.preventDefault();
+});
